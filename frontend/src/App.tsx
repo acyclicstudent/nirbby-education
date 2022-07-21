f13c7927a6637694dea1e85ca869e6db4907f577
AxelAdrian2002 <60492125+AxelAdrian2002@users.noreply.github.com>
2022-07-21 00:55:52 -0500

    creacion proyecto react typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom';
+import './App.css';
+import Home from './routes/Home';
+import Auth from './routes/Auth';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <Switch>
+      {
+        isLoggedIn ? (
+            <Route exact path="/" component={Home} />
+        ) : (
+          <Route exact path="/" component={Auth} />
+        )
+      }
+    </Switch>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
