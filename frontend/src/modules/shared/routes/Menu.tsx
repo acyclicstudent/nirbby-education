70a3667b8f4969fed5f30872f67015e67c3203c6
DanAurMG <raizeremi.15.damg@gmail.com>
2022-07-22 13:00:27 -0500

    Creacion de la nav var y diseno del auth

--- frontend/src/modules/shared/routes/Menu.tsx
+++ frontend/src/modules/shared/routes/Menu.tsx
@@ -1,3 +1,20 @@
-export default function Menu() {
 
+import './sass/Menu.scss';
+
+const imageLogo = require("../../assets/images/logo.png");
+
+export default function Menu () {
+    return (
+        <div className="components-ui-menu">
+            <div className="left">
+                <img src={imageLogo} alt='Boton de menú'  className="image"/>
+                <p>Mascotas pendejas</p>
+            </div>
+            <div className="right">
+                <div className="begin"><p>Inicio</p></div>
+                <div className="upload"><p>Subir mascota</p></div>
+                <div className='logout'><p>LogOut</p></div>
+            </div>
+        </div>
+    );
 }
