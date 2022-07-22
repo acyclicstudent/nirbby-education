6426301e15e327bb58b345173b9e5c8daaa192f9
DanAurMG <raizeremi.15.damg@gmail.com>
2022-07-21 21:58:21 -0500

    MAs cambios para que funcione el flujo del programa

--- frontend/src/modules/default/DefaultRouter.tsx
+++ frontend/src/modules/default/DefaultRouter.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../shared/routes/Auth";
 import AuthForm from "./components/forms/AuthForm";
+import Home from "./routes/Home";
 
 export default function CoachesRouter() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <Switch>
-            <Route 
-                exact 
-                path="/app" 
-                render={() => <Auth type="default" AuthForm={AuthForm} />} 
-            />
+            {
+                isLoggedIn ? (
+                    <>
+                        <Route
+                            exact
+                            path="/institutes"
+                            component={Home}
+                            render={() => <Auth type="institutes" AuthForm={AuthForm} />}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Route
+                            exact
+                            path="/app"
+                            render={() => <Auth type="default" AuthForm={AuthForm} />}
+                        />
+                    </>
+                )
+            }
         </Switch>
     );
 }
