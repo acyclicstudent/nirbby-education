4f8ebeae8a32fad61aa5082ffda832ba64d7e764
AxelAdrian2002 <60492125+AxelAdrian2002@users.noreply.github.com>
2022-07-21 21:02:43 -0500

    Cambios commit

--- frontend/src/modules/institutes/components/forms/AuthForm.tsx
+++ frontend/src/modules/institutes/components/forms/AuthForm.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import LoginForm from "../LoginForm";
+import RegisterForm from "../RegisterForm";
+
 export default function AuthForm(){
+    const [isLogin, setIsLogin] = useState(true);
+
     return (
         <div className="institutes-components-forms-authform">
-            <p>AuthForm Institutes</p>
-            
+            {
+                isLogin ? (
+                    <LoginForm />
+                ) : (
+                    <RegisterForm />
+                )
+            }
         </div>
-    )
+    );
 }
