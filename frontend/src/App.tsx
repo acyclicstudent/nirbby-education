8dd4c89da593141bb041a901b2e0e87a84f9f31e
DanAurMG <raizeremi.15.damg@gmail.com>
2022-07-21 21:06:53 -0500

    Creacion de archivos necesarios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import Home from './routes/Home';
 import Auth from './modules/shared/routes/Auth';
 import CoachesModule from './modules/coaches';
 import InstitutesModule from './modules/institutes';
+import DefaultModule from './modules/default';
 import Landing from './routes/Landing';
 
 function App() {
---
+++ frontend/src/modules/default/DefaultRouter.tsx
@@ -0,0 +1,15 @@
+import { Route, Switch } from "react-router-dom";
+import Auth from "../shared/routes/Auth";
+import AuthForm from "./components/forms/AuthForm";
+
+export default function CoachesRouter() {
+    return (
+        <Switch>
+            <Route 
+              exact 
+              path="/app" 
+              render={() =>  <Auth type="coaches" AuthForm={AuthForm} />} 
+            />
+        </Switch>
+    );
+}
