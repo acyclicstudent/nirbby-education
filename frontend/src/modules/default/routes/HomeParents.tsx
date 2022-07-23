ecc8baffe71471647bd786aab6d9875a403c83c4
DanAurMG <raizeremi.15.damg@gmail.com>
2022-07-22 22:09:29 -0500

    Uso del nuevo circulrbar

--- frontend/src/modules/default/routes/HomeParents.tsx
+++ frontend/src/modules/default/routes/HomeParents.tsx
@@ -14,15 +14,15 @@ export default function HomeParents() {
                         <p>Progreso de mis estudiantes</p>
                         
                         <div className="alumno">
-                            <CircularBar value={89} text={"89"}/>
+                            <CircularBar value={89} text={"89"} width={80} height={80}/>
                             <p>Nombre de un vato</p>
                         </div>
                         <div className="alumno">
-                            <CircularBar value={45} text={"45"}/>                        
+                            <CircularBar value={45} text={"45"} width={120} height={100}/>                        
                             <p>Nombre de otro vato</p>
                         </div>
                         <div className="alumno">
-                            <CircularBar value={25} text={"25"}/>
+                            <CircularBar value={25} text={"25"} width={40} height={60}/>
                             <p></p>
                         </div>
                     </div>
