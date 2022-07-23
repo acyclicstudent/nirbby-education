8b06145b7a822879b8b849c86049c966a14db44c
AxelAdrian2002 <60492125+AxelAdrian2002@users.noreply.github.com>
2022-07-22 20:11:43 -0500

    creacion de no hay rifa y correcion de homestyudent

---
+++ frontend/src/modules/default/components/forms/NoPrize.tsx
@@ -0,0 +1,13 @@
+import './sass/NoPrize.scss';
+
+export default function NoPrize(){
+    return(
+        <div className="app-component-noprize">
+            <p className="prize">Rifa</p>
+            <div className="app-component-nolap">
+                <p className="app-component-name">POR EL MOMENTO NO HAY RIFA</p>
+                
+            </div>
+        </div>
+    )
+}
