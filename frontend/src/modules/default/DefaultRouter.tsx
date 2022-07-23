0253e740a3c27eecb5338139a1378f8871e48c96
Acyclicstudent <axel_07_avila@hotmail.com>
2022-07-23 02:51:24 -0500

    Fixes y cambios en default router

--- frontend/src/modules/default/DefaultRouter.tsx
+++ frontend/src/modules/default/DefaultRouter.tsx
@@ -4,24 +4,35 @@ import Auth from "../shared/routes/Auth";
 import AuthForm from "./components/forms/AuthForm";
 import Rewards from "./routes/Rewards";
 import HomeParents from "./routes/HomeParents";
+import HomeStudent from "./routes/HomeStudent";
 
 export default function DefaultRouter() {
-    //const [state, authDispatchers] = useAuth('default');
-    const [state, authDispatchers] = [{isAuth: true}, {}]
+    const [state, authDispatchers] = useAuth('default');
+    console.log(state);
     return (
         <AuthProvider value={authDispatchers as any}>
             <Switch>
                 {
                     state.isAuth ? (
-                        <>
-                            <Route
-                                exact
-                                path="/app"
-                                component={HomeParents}
-                                render={() => <Auth type="default" AuthForm={AuthForm} />}
-                            />
-                            <Route path="/app/afiliates" component={Rewards} />
-                        </>
+                        state.isParent ? (
+                            <>
+                                <Route
+                                    exact
+                                    path="/app"
+                                    component={HomeParents}
+                                />
+                                <Route path="/app/afiliates" component={Rewards} />
+                            </>
+                            ) : (
+                            <>
+                                <Route
+                                    exact
+                                    path="/app"
+                                    component={HomeStudent}
+                                />
+                                <Route path="/app/afiliates" component={Rewards} />
+                            </>
+                        )
                     ) : (
                         <>
                             <Route
